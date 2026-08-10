628e9fdcae6e947ab15a767d4b67ce093d56c32f
SANYA ANAND <sanya.anand294@gmail.com>
2026-08-10 10:58:20 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq[26] = {0};
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i = 0; i<s.length(); i++)
+        {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+
+        for(int num : freq)
+        {
+            if (num != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
