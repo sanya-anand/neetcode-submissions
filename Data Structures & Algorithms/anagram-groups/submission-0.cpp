40897dd91da1dcfdf7c845f1b945f85b0b669ed6
SANYA ANAND <sanya.anand294@gmail.com>
2026-08-11 14:34:31 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            int freq[26] = {0};
+
+            for (char c : s)
+                freq[c - 'a']++;
+
+            string key;
+
+            for (int i = 0; i < 26; i++)
+                key += to_string(freq[i]) + "#";
+
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for (auto& pair : mp)
+            ans.push_back(pair.second);
+
+        return ans;
+    }
+};
